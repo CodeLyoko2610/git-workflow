3438d132b1e5ff94cb894c165735c3503f5810e7
Dang Chuong Pham <chuong.pham@kempower.com>
2023-09-09 11:39:20 +0300

    add helper comments and logs

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+//---------------Function declarations------------------
 const createRecipe = function (name, instructions) {
   return {
     name, //Pancake
@@ -7,12 +8,14 @@ const createRecipe = function (name, instructions) {
 
 const withMetrics = function (time, calories) {
   return function (recipe) {
-    const { name, instructions } = recipe;
+    const { name, instructions } = recipe; // returned function accepting parameter
 
     return {
       name,
       instructions,
       printInstructions: function () {
+        // console.log(`DEBUG: context`, this);
+
         console.log(
           `Instructions for ${this.name}: ${this.instructions} for ${time} seconds. Contain ${calories} calories`
         );
@@ -21,11 +24,12 @@ const withMetrics = function (time, calories) {
   };
 };
 
+//------------------Run------------------
 const createRecipeResult = createRecipe(
   "Pancakes",
   "Mix flour, eggs, and milk. Cook on a griddle."
 );
 
-const pancakeRecipe = withMetrics(30, 200)(createRecipeResult);
+const pancakeRecipe = withMetrics(30, 200)(createRecipeResult); // withMetrics return function. Function requires param.
 
 pancakeRecipe.printInstructions();
